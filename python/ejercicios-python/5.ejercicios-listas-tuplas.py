4266b73a73d10170941aa804f32805552d15e2b7
cristianl0pez <cristianlopezvalpo@gmail.com>
2022-02-14 19:39:23 -0300

    ejercicios python

---
+++ python/ejercicios-python/5.ejercicios-listas-tuplas.py
@@ -0,0 +1,64 @@
+
+#listas y tuplas
+
+
+
+"""Ejercicio 1
+Escribir un programa que almacene las asignaturas de un curso (por ejemplo Matemáticas, Física, Química, Historia y Lengua) en una lista y la muestre por pantalla."""
+
+
+"""Ejercicio 2
+Escribir un programa que almacene las asignaturas de un curso (por ejemplo Matemáticas, Física, Química, Historia y Lengua) 
+en una lista y la muestre por pantalla el mensaje Yo estudio <asignatura>, donde <asignatura> es cada una de las asignaturas de la lista.
+"""
+
+"""Ejercicio 3
+Escribir un programa que almacene las asignaturas de un curso (por ejemplo Matemáticas, Física, Química, Historia y Lengua) en una lista, 
+pregunte al usuario la nota que ha sacado en cada asignatura, y después las muestre por pantalla con el mensaje
+ En <asignatura> has sacado <nota> donde <asignatura> es cada una des las asignaturas de la lista y <nota> cada una de las correspondientes notas introducidas por el usuario."""
+
+
+"""Ejercicio 4
+Escribir un programa que pregunte al usuario los números ganadores de la lotería primitiva, los almacene en una lista y los muestre por pantalla ordenados de menor a mayor."""
+
+
+"""Ejercicio 5
+Escribir un programa que almacene en una lista los números del 1 al 10 y los muestre por pantalla en orden inverso separados por comas."""
+
+
+"""Ejercicio 6
+Escribir un programa que almacene las asignaturas de un curso (por ejemplo Matemáticas, Física, Química, Historia y Lengua) en una lista, 
+pregunte al usuario la nota que ha sacado en cada asignatura y elimine de la lista las asignaturas aprobadas.
+ Al final el programa debe mostrar por pantalla las asignaturas que el usuario tiene que repetir."""
+
+
+"""Ejercicio 7
+Escribir un programa que almacene el abecedario en una lista, elimine de la lista las letras que ocupen posiciones múltiplos de 3, y muestre por pantalla la lista resultante."""
+
+
+"""Ejercicio 8
+Escribir un programa que pida al usuario una palabra y muestre por pantalla si es un palíndromo."""
+
+
+"""Ejercicio 9
+Escribir un programa que pida al usuario una palabra y muestre por pantalla el número de veces que contiene cada vocal."""
+
+
+"""Ejercicio 10
+Escribir un programa que almacene en una lista los siguientes precios, 50, 75, 46, 22, 80, 65, 8, y muestre por pantalla el menor y el mayor de los precios."""
+
+
+"""Ejercicio 11
+Escribir un programa que almacene los vectores (1,2,3) y (-1,0,2) en dos listas y muestre por pantalla su producto escalar."""
+
+"""Ejercicio 12
+Escribir un programa que almacene las matrices
+ 
+
+en una lista y muestre por pantalla su producto.
+Nota: Para representar matrices mediante listas usar listas anidadas, representando cada vector fila en una lista.
+"""
+
+
+"""Ejercicio 13
+Escribir un programa que pregunte por una muestra de números, separados por comas, los guarde en una lista y muestre por pantalla su media y desviación típica."""
---
+++ python/ejercicios-python/7.ejercicios-funciones.py
@@ -0,0 +1,50 @@
+ #Ejercicios de Funciones
+
+
+"""Ejercicio 1
+Escribir una función que muestre por pantalla el saludo ¡Hola amiga! cada vez que se la invoque."""
+
+
+"""Ejercicio 2
+Escribir una función a la que se le pase una cadena <nombre> y muestre por pantalla el saludo ¡hola <nombre>!."""
+
+
+"""Ejercicio 3
+Escribir una función que reciba un número entero positivo y devuelva su factorial."""
+
+
+"""Ejercicio 4
+Escribir una función que calcule el total de una factura tras aplicarle el IVA.
+ La función debe recibir la cantidad sin IVA y el porcentaje de IVA a aplicar, y devolver el total de la factura. 
+ Si se invoca la función sin pasarle el porcentaje de IVA, deberá aplicar un 21%."""
+
+
+"""Ejercicio 5
+Escribir una función que calcule el área de un círculo y otra que calcule el volumen de un cilindro usando la primera función."""
+
+
+"""Ejercicio 6
+Escribir una función que reciba una muestra de números en una lista y devuelva su media."""
+
+
+"""Ejercicio 7
+Escribir una función que reciba una muestra de números en una lista y devuelva otra lista con sus cuadrados."""
+
+
+"""Ejercicio 8
+Escribir una función que reciba una muestra de números en una lista y devuelva un diccionario con su media, varianza y desviación típica."""
+
+
+"""Ejercicio 9
+Escribir una función que calcule el máximo común divisor de dos números y otra que calcule el mínimo común múltiplo."""
+
+
+"""Ejercicio 10
+Escribir una función que convierta un número decimal en binario y otra que convierta un número binario en decimal."""
+
+
+"""Ejercicio 11
+Escribir un programa que reciba una cadena de caracteres y devuelva un diccionario con cada palabra que contiene y su frecuencia.
+ Escribir otra función que reciba el diccionario generado con la función anterior y devuelva una tupla con la palabra más repetida y su frecuencia."""
+
+
