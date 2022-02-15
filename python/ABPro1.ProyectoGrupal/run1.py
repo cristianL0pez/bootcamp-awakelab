3f44508aadd7bdba439024b1403944c3f0e86243
cristianl0pez <cristianlopezvalpo@gmail.com>
2022-02-15 18:41:27 -0300

    ABpro1 entregado

--- python/ABPro1.ProyectoGrupal/run1.py
+++ python/ABPro1.ProyectoGrupal/run1.py
@@ -3,7 +3,8 @@
 ● Activación del entorno virtual creado.
 ● Investigar las ventajas que tenemos al utilizar un entorno virtual en el desarrollo de nuestro
 proyecto.
-● Como grupo, deberán definir un listado de 5 productos con su respectivo valor unitario.
+Como grupo, deberán definir un listado de 5 productos con su respectivo valor unitario.
+
 ● Deberán crear un archivo .py el cual deberá solicitar ingresar una cantidad por cada producto
 (definido de la lista).
 ● Se deberá mostrar en pantalla el total de la suma del pedido el que corresponde al valor neto.
@@ -17,36 +18,39 @@ En caso que no tengan instalado Word, pueden apoyarse en GoogleDocs.
 - El tiempo máximo para resolver la evaluación es el periodo correspondiente a una clase regular.
 - Equipos máximos de 4 integrantes.
 """
-import inquirer 
-questions = [
-  inquirer.List('autos',
-                message="que auto quieres comprar?",
-                choices=['audi', 'foton', 'dodge', 'chevrolet', 'bmw',]
-            ),
-]
-answers = inquirer.prompt(questions)
-auto = answers
-cantidad = input("escriba la cantidad  :")
+from xml.dom import DomstringSizeErr
 
 
-autos = {
-    'nombre':'',
-    'precio':00
-}
+productos=[['auto1',29],['auto2',117],['auto3',10],['auto4',300],['auto5',1000]]
 
-inventario =[]
-
-inventario.append({ 'nombre':'audi','precio': 25,  })
-inventario.append({ 'nombre':'foton','precio':20,  })
-inventario.append({ 'nombre':'dodge','precio': 23,  })
-inventario.append({ 'nombre':'chevrolet','precio': 18,  })
-inventario.append({ 'nombre':'bmw','precio': 35,  })
+auto1=input('cuantos auto 1 quiere de 29 : ')
+auto2=input('cuantos auto 2 quiere de 117: ')
+auto3=input('cuantos auto 3 quiere de 10: ')
+auto4=input('cuantos auto 4 quiere de 300: ')
+auto5=input('cuantos auto 5 quiere de 1000: ')
 
 
-for p in inventario:
-    print('___________')
-    print('\t nombre:',p['nombre'])
-    print('\t precio:',p['precio'])
+uno=productos[0][1]
+dos=productos[1][1]
+tres=productos[2][1]
+cuatro=productos[3][1]
+cinco=productos[4][1]
+
+
+
+uno = int(auto1) * uno 
+dos =  int(auto2)*dos
+tres = int(auto3)*tres
+cuatro=int(auto4)*cuatro
+cinco = int(auto5)*cinco
+resutadoNeto= uno+dos+tres+cuatro+cinco
+
+
+resultadoIva=resutadoNeto*119/100
+print(resultadoIva)
+
+
+
 
 
 
