248a6ca5f170a934679f688e45050778e76183ea
cristianl0pez <cristianlopezvalpo@gmail.com>
2022-02-21 23:57:47 -0300

    trabajo grupal

---
+++ python/APB4/ABPro4.ProyectoGrupal/run.py
@@ -0,0 +1,75 @@
+'''Hoy simularemos que nuestra tienda virtual tiene muchos usuarios comprando desesperadamente. De
+igual forma, simularemos un alto movimiento de proveedores que renuevan nuestro stock de productos
+a ofrecer.
+Primero, desarrollaremos una forma de almacenar nuestro stock de dos productos. El primer producto
+tendrá 120 unidades y el segundo 150.
+Luego, simularemos cada 3 segundos una compra de “n” unidades de cualquiera de los dos productos.
+n representa un número aleatorio entre 1 y 10.
+Cada compra, como es natural, afecta el stock inicial de productos. Es decir, si una compra simulada es
+de 3 unidades del producto 1, este se debe descontar del stock.
+Cada 10 compras, el programa debe imprimir en pantalla el número de unidades disponibles por producto.
+¿Lo lograron?
+Por último, cuando un producto tenga un stock de menos de 100 unidades, los proveedores enviarán
+automáticamente 50 unidades más. Esto se debe reflejar en el stock de cada producto.
+Lamentablemente, los proveedores solo tienen stock suficiente para enviar 150 unidades en total de
+productos 1 y 2.
+¡Buena suerte desarrolladores!'''
+
+import random
+import time
+
+producto1=150
+producto2=120
+abast=150
+def aleatoreo(valor_inf,valor_sup):
+    return random.randint(valor_inf,valor_sup)
+
+for i in range(1,20):
+    producto=aleatoreo(1,2)
+    carrito=aleatoreo(10,20)
+    if producto == 1:
+        time.sleep(3)
+        print('_______________________________')
+        
+        print('el stock del producto 1 es : ',producto1)
+        print('se han vendido:',carrito,'articulos','-',producto1)
+        producto1 -= carrito
+        print('el stock del producto 1 es : ',producto1)
+        print('_______________________________')
+        if producto1 <= 100 and abast>0:
+             abast-=50
+             producto1+=50
+        elif abast==0:
+            print('el abastecimiento en 0')
+        
+             
+             
+    else:
+        time.sleep(3)
+        print('_______________________________')
+        
+        print('el stock del producto 2 es : ',producto2)
+        print('se han vendido :',carrito,'articulos','-',producto2)
+        producto2 -= carrito
+        print('el stock del producto 2 es : ',producto2)
+        print('_______________________________')
+        if producto2 <= 100 and abast>0:
+             abast-=50
+             producto2+=50
+        elif abast==0:
+            print('el abastecimiento en 0')
+        
+        
+        
+        
+
+
+
+
+
+    
+    
+
+
+
+
---
+++ python/APB4/ABPro4.ProyectoIndividual/run.py
@@ -0,0 +1,31 @@
+'''Familiarizado ya con estos componentes debemos prepararnos para realizar las siguientes acciones,
+para simular el funcionamiento de tu aplicación.
+OPCIÓN 1:
+Nuestra aplicación tendrá la siguiente particularidad. Al momento de crear un usuario, este debe ingresar
+una contraseña que esté acorde a nuestros criterios de seguridad.
+En este sentido, la contraseña debe contar con al menos 8 caracteres, con mayúsculas, minúsculas y
+cifras.
+Nuestro programa debe indicarle al usuario qué criterios le faltan para disponer de una contraseña
+segura.
+La contraseña debe ingresarse carácter por carácter en el terminal. Luego de escribir cada carácter, el
+programa envía un mensaje con los criterios aún incumplidos.
+OPCIÓN 2:
+¿Recuerdan que en el ejercicio pasado asignamos cuestionarios particulares para cada perfil? Acá un
+pequeño recordatorio: Para desarrollar de mejor forma tu aplicación, requieres conocer mejor a los
+usuarios que la utilizarán. Antes de continuar desarrollando, debes elaborar un programa que tiene la
+funcionalidad de enviar cuestionarios a grupos particulares de personas.
+Nuestro socio consideró que los cuestionarios diferenciados estaban impactando negativamente en la
+información obtenida.
+En este sentido, nos pidió que distribuyamos los cuestionarios de forma aleatoria, sin tomar en
+consideración la información personal.
+Esta vez tomamos una serie de decisiones para facilitar el proceso:
+Solo 7 personas responderán cuestionarios.
+Tendremos los mismos cuestionarios del ejercicio anterior:
+● Hábitos alimenticios, experiencia laboral, actividades recreativas, atletismo, natación y deportes
+en general.
+Los requisitos son:
+● Los cuestionarios se envían de forma aleatoria.
+● Los cuestionarios se envían uno después del otro con un desfase de 3 segundos para no
+sobrecargar el servidor.
+● El número máximo de formularios que una persona puede recibir es de 5.
+Almacene el nombre del formulario que va a contestar cada persona.'''
