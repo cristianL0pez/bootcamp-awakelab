e8947e782508b7cc6dcee5fcbf4aec1d0687667b
cristianl0pez <cristianlopezvalpo@gmail.com>
2022-02-24 22:54:47 -0300

    trabajo 5

--- python/APB5/ABPro5.ProyectoGrupal/run.py
+++ python/APB5/ABPro5.ProyectoGrupal/run.py
@@ -23,47 +23,75 @@ ayudará a otras personas a comprender su código.
 Imprima en pantalla un listado que contenga los ID de los usuarios.
 Modifique todos los ID. Agregue la siguiente cadena de caracteres: “_piloto” al final de cada ID.
 Imprima en pantalla los nuevos ID.
-Elimine los últimos cuatro ID_clientes en el listado.'''
+Elimine los últimos cuatro ID_clientes en el listado.
 
-'''clientes=[]
-productos=[]
-
-print('agregar clientes')
-client=input('nombre del cliente : ')
-clientes.append(client)
-    
-print('agregar clientes')
-product=input('nombre del producto : ')
-productos.append(product)
-    
-print('mostrar clientes : ')
-print(clientes)
-print('mostrar productos : ')
-print(productos)
-print('eliminar clientes : ')
-name=input('nombre del cliente : ')
-clientes.remove(name)
-
-print('eliminar productos')
-namep=input('nombre del producto : ')
-productos.remove(namep)
 ● Información de clientes: nombre, edad, identificador único.
 ● Información de productos: nombre, precio, identificador único y color.
 ● Información de la compra de cada cliente.'''
+''' Agregar Cliente.
+● Agregar Producto
+● Mostrar Clientes: Muestra el listado de clientes.
+● Mostrar Producto: Muestra el listado de productos.
+● Elimine clientes. ¿Qué información requiere para eliminar un cliente al azar?
+● Elimine productos. ¿Qué información requiere para eliminar el último producto agregado?'''
+
+
+import requests
+import json
+
 
 #https://randomuser.me/documentation#howto documentacion api
 #https://fakestoreapi.com/docs
 
+url = 'https://randomuser.me/api/?results=10&inc=name,dob,id' #url de la api
+data =  requests.get(url)                                     #request generica 
+users = data.json()['results']                                #tranformo data en json
 
-import requests
-import json 
 
-URL ='https://randomuser.me/api/?results=10,name,nat'
-data = requests.get(URL)
-data = data.json()
-usuario = data
+#url2 = ""
+#data2 = requests.get(url2)
+#products = data2.json()
 
-print(usuario)
+
+#productos = []                                                
+#for producto in products:
+   # title = f"{productos['title']}"
+   # productos.append({'title': title,})
+
+
+clients = []                                                
+for user in users:
+    name = f"{user['name']['first']} {user['name']['last']}"
+    age = user['dob']['age']
+    id = user['id']['value']
+    clients.append({'name': name, 'age': age, 'id': id})
+    
+    
+
+print(clients[0])
+print(clients)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
