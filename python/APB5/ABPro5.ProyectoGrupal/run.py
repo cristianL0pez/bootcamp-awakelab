91f592e4d9cde89d2db3a5d24ce80e53f58e01b8
cristianl0pez <cristianlopezvalpo@gmail.com>
2022-02-25 22:10:10 -0300

    trabajo 5

--- python/APB5/ABPro5.ProyectoGrupal/run.py
+++ python/APB5/ABPro5.ProyectoGrupal/run.py
@@ -23,22 +23,30 @@ ayudará a otras personas a comprender su código.
 Imprima en pantalla un listado que contenga los ID de los usuarios.
 Modifique todos los ID. Agregue la siguiente cadena de caracteres: “_piloto” al final de cada ID.
 Imprima en pantalla los nuevos ID.
-Elimine los últimos cuatro ID_clientes en el listado.
+Elimine los últimos cuatro ID_clientes en el listado.'''
 
-● Información de clientes: nombre, edad, identificador único.
-● Información de productos: nombre, precio, identificador único y color.
-● Información de la compra de cada cliente.'''
-''' Agregar Cliente.
-● Agregar Producto
-● Mostrar Clientes: Muestra el listado de clientes.
-● Mostrar Producto: Muestra el listado de productos.
-● Elimine clientes. ¿Qué información requiere para eliminar un cliente al azar?
-● Elimine productos. ¿Qué información requiere para eliminar el último producto agregado?'''
-
-
+import time
 import requests
 import json
+import uuid
+import os
+import sys
+from pprint import pprint
 
+sys.path.append(os.path.realpath("."))
+import inquirer  # noqa
+
+questions = [
+    inquirer.List(
+        "size",
+        message="elija una opcion",
+        choices=["1. Agregar Cliente", "2. Agregar Producto", "3. Mostrar Clientes:", "4. Mostrar Producto", "5. Elimine clientes", "6. Elimine productos","7. Elimine 4 ultimos",'8. agregar _piloto','9. mostrar ids','10. mostrar valores','11. mostrar keys'],
+    ),
+]
+
+answers = inquirer.prompt(questions)
+
+pprint(answers)
 
 #https://randomuser.me/documentation#howto documentacion api
 #https://fakestoreapi.com/docs
@@ -46,30 +54,136 @@ import json
 url = 'https://randomuser.me/api/?results=10&inc=name,dob,id' #url de la api
 data =  requests.get(url)                                     #request generica 
 users = data.json()['results']                                #tranformo data en json
-
-
-#url2 = ""
-#data2 = requests.get(url2)
-#products = data2.json()
-
-
-#productos = []                                                
-#for producto in products:
-   # title = f"{productos['title']}"
-   # productos.append({'title': title,})
-
-
-clients = []                                                
+####################################################################
+#creo la lista clients y productos vacia
+clients = []  
+productos = [{'name': 'auto', 'id': ''}]                                               
 for user in users:
     name = f"{user['name']['first']} {user['name']['last']}"
     age = user['dob']['age']
-    id = user['id']['value']
-    clients.append({'name': name, 'age': age, 'id': id})
+    id1 = uuid.uuid4()
+    id1 = str(id1)
     
+    clients.append({'name': name, 'age': age, 'id': id1})
+
+
     
 
-print(clients[0])
-print(clients)
+######################################################################
+#agregando bmostrando y borrando
+opcion = answers['size']
+oadd1='1. Agregar Cliente'
+oadd2='2. Agregar Producto'
+oadd3='3. Mostrar Clientes:'
+oadd4='4. Mostrar Producto'
+oadd5='5. Elimine clientes'
+oadd6='6. Elimine productos'
+oadd7='7. Elimine 4 ultimos'
+oadd8='8. agregar _piloto'
+oadd9='9. mostrar ids'
+oadd10='10. mostrar valores'
+oadd11='11. mostrar keys'
+
+if opcion == oadd1:
+   nameadd = input('ingrese el nombre: ')
+   id = uuid.uuid4()
+   id_str = id
+   str(id_str)
+   clients.append({'name': nameadd, 'age': age, 'id': id})
+   print(clients[-1])
+
+elif oadd2 == opcion:
+   nameadd = input('ingrese el nombre del producto ')
+   id = uuid.uuid4()
+   id_str = id
+   str(id_str)
+   productos.append({'name': nameadd, 'id': id_str})
+   print(productos)
+
+elif oadd3 == opcion:
+     print(clients)
+
+elif oadd4 == opcion:
+    print(productos)
+
+elif oadd5 == opcion:
+    print(clients.pop())
+  
+
+elif oadd6 == opcion:
+    print(productos.pop())
+
+elif oadd7 == opcion:
+    eliminar = len(clients) - (4 + 1)
+    for index, client in enumerate(clients):
+         if index > eliminar:
+            del client['id']
+            print(clients)
+
+elif oadd8 == opcion:
+    for client in clients:
+        client['id'] += '_piloto'
+        print(client)
+
+elif oadd9 == opcion:
+    for client in clients:
+        print(client['id'])
+
+elif oadd10 == opcion:
+    for client in clients:
+        for value in client.values():
+            print(value)
+
+elif oadd11 == opcion:
+    for key in clients[0].keys():
+        print(key)
+    
+
+ 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        
+
+
+   
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+                                           
+
+
+
+
+
 
 
 
---
+++ python/APB5/ABPro5.ProyectoIndividual/run.py
@@ -0,0 +1,79 @@
+'''DESARROLLO
+Como buen desarrollador, para comenzar a poder trabajar de manera óptima, es necesario que debamos
+preparar las herramientas necesarias para inicializar nuestro proyecto, esto incluye tener ya nuestro
+editor de texto y la versión de Python disponible en nuestro equipo.
+Familiarizado ya con estos componentes debemos prepararnos para realizar las siguientes acciones,
+para simular el funcionamiento de tu aplicación.
+
+
+
+Diseñe 7 diccionarios, donde el nombre de cada diccionario es el nombre de un usuario de su aplicación.
+En cada diccionario, integre características como: edad, género y otras características particulares de su
+aplicación.
+
+
+Por ejemplo, si la aplicación se enfoca en Juntas de Vecinos integrar dirección y número telefónico.
+Integre al menos cinco características.
+Guarde estos diccionarios en una lista. En el caso de ejemplo, podría ser nombrada “JJVV”.
+A continuación, recorra su lista e imprima toda la información que posee la estructura de datos sobre
+cada usuario (en el caso de ejemplo: de cada junta de vecinos).
+¿Qué problemas encontró con esta forma de almacenar la información?
+Vuelva al inicio del problema y diseñe una estructura de datos unificada que permita almacenar todas
+las juntas de vecinos.
+Agregue para cada usuario los campos nombre_usuario, id_unico, antigüedad, fecha de incorporación.
+Imprima en pantalla la fecha de incorporación de todos los usuarios.'''
+
+import time
+import requests
+import json
+import uuid
+import os
+import sys
+###from pprint import pprint
+
+'''sys.path.append(os.path.realpath("."))
+import inquirer  # noqa
+
+questions = [
+    inquirer.List(
+        "size",
+        message="elija una opcion",
+        choices=["1. Agregar Cliente", "2. Agregar Producto", "3. Mostrar Clientes:", "4. Mostrar Producto", "5. Elimine clientes", "6. Elimine productos","7. Elimine 4 ultimos",'8. agregar _piloto','9. mostrar ids','10. mostrar valores','11. mostrar keys'],
+    ),
+]
+
+answers = inquirer.prompt(questions)
+
+pprint(answers)'''
+
+
+url = 'https://randomuser.me/api/?results=7&inc=name,dob,id,location,phone,registered' #url de la api
+data =  requests.get(url)                                     #request generica 
+users = data.json()['results']                                #tranformo data en json
+####################################################################
+#creo la lista clients y vacia
+jjvv = []                                                
+for user in users:
+    name = f"{user['name']['first']} {user['name']['last']}"
+    age = user['dob']['age']
+    direccion = user['location']['street']
+    telefono = user['phone']
+    registrado = user['registered']['date']
+    id1 = uuid.uuid4()
+    id1 = str(id1)
+    jjvv.append({'name': name, 'age': age, 'id': id1,'street':direccion,'phone': telefono,'date':registrado})
+    print(jjvv)
+
+for d in jjvv:
+    print('________________')
+    print(d)
+    print('________________')
+
+for d in jjvv:
+    print('registro')
+    print('______________')
+    print(d['name'])
+    print(d['date'])
+    
+    
+
