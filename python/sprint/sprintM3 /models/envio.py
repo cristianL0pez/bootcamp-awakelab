1b23d1ae76735755d6a84214aff36915692c8ff0
cristianl0pez <cristianlopezvalpo@gmail.com>
2022-03-05 22:12:40 -0300

    correccion de codigo sprint

--- python/sprint/sprintM3 /models/envio.py
+++ python/sprint/sprintM3 /models/envio.py
@@ -3,27 +3,27 @@ bodega_a = [{}]
 bodega_b = [{}]
 def envios():
     cliente = input('ingrese el cliente')
-    producto=input('ingrese el producto')
+    producto1=input('ingrese el producto')
     numero_producto = int(input('ingrese numero de producto'))
     direccion = input('ingrese la direccion')
     distancia = int(input('ingrese la distancia de envio'))
     if distancia > 1000:
-        print(bodega_b)
-        bodega_b.append({'cliente':cliente,'producto':producto,'numero':numero_producto, 'direccion':direccion})
+        bodega_b.append({'cliente':cliente,'producto':producto1,'numero':numero_producto, 'direccion':direccion})
         print('el producto',bodega_b[-1],'sera enviado a la bodega b ')
         for producto in bodega_b:
-            if producto>500:
+            if numero_producto>500:
+                bodega_b.pop(-1)
                 print('el limite de la bodega es 500 unidades por producto')
     else:
         print(bodega_a)
-        bodega_a.append({'cliente':cliente,'producto':producto,'direccion':direccion})
-        print(f'el producto',{bodega_b[-1]},'sera enviado a la bodega a ')
-        for producto in bodega_b:
-            if producto>500:
+        bodega_a.append({'cliente':cliente,'producto':producto1,'numero':numero_producto,'direccion':direccion})
+        print('el producto',bodega_a[-1],'sera enviado a la bodega a ')
+        for producto in bodega_a:
+            if numero_producto>500:
                 print('el limite de la bodega es 500 unidades por producto')
 
 
-    
+
 
 
 
