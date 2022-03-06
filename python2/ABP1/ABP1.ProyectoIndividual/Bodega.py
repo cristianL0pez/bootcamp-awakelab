ea357533068ddf2853d152863fdd6e3b127eb364
cristianl0pez <cristianlopezvalpo@gmail.com>
2022-03-06 17:08:18 -0300

    trabajo individual modulo 4 en to-do

---
+++ python2/ABP1/ABP1.ProyectoIndividual/Bodega.py
@@ -0,0 +1,18 @@
+class Bodega:
+
+    def __init__(self,_id,productos) -> None:
+        self._id = _id
+        self.productos = productos
+        
+    def mandar_productos():
+        print('mandar_productos')  
+        pass
+
+    def resivir_productos():
+        print('resivir_productos')
+        pass
+
+
+    def guardar_productos():
+        print('guardar_productos')
+        pass
