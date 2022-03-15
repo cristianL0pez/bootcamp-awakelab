1a04ca4b8003fd981fddc7fb48ce16a6c90ec251
cristianl0pez <cristianlopezvalpo@gmail.com>
2022-03-14 23:10:43 -0300

    guardando en lista

--- python2/ABP2/ABP2.ProyectoGrupal/classes/Cliente.py
+++ python2/ABP2/ABP2.ProyectoGrupal/classes/Cliente.py
@@ -1,12 +1,14 @@
 
 
 
+
+
+
 class Cliente:
-    lista_clientes = []
-    
+    lista_clientes=[]    
 
 
-    def __init__(self,__id,nombre,correo,Fecha_Registro,__Saldo):
+    def __init__(self,__id,nombre,correo,Fecha_Registro,__Saldo,):
         self.__id = __id
         self.nombre = nombre
         self.correo = correo
@@ -14,28 +16,27 @@ class Cliente:
         self.__Saldo = __Saldo
         
         
-
-        
-  
-        
-        
-
-    
-    def guardar_lista_cliente(self):
-        for cliente in Cliente.lista_clientes:
-            print(cliente)
-
-        
-
-
-
     def obtener_cliente(self):
         return self.__dict__
+    
+    def guardar_lista_cliente(self):
+         Cliente.lista_clientes.append(self.__dict__)
+
+    
+         
+         
+         
+        
+
 
     
 
-cliente1=Cliente('adjakshld','adjakshld','adjakshld','adjakshld','adjakshld',)
+    
+
+cliente1=Cliente('adjakshld','adjakshld','adjakshld','adjakshld','adjakshld')
+print(cliente1.obtener_cliente())
 cliente1.guardar_lista_cliente()
+print( Cliente.lista_clientes)
 
  
             
