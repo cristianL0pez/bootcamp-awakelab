a154c308a21ba6f169479c76d28e1a1c5d123c55
cristianl0pez <cristianlopezvalpo@gmail.com>
2022-03-26 19:12:49 -0300

    bases de datos grupal 2

--- bases-de-datos/ABP2/ABP2.ProyectoGrupal/telovendo.sql
+++ bases-de-datos/ABP2/ABP2.ProyectoGrupal/telovendo.sql
@@ -16,5 +16,6 @@ SELECT SUM(precio) FROM  `Productos`;
 
 SELECT nombre, precio from `Productos` WHERE stock > 1;
 
+SELECT comuna, COUNT(comuna) AS c , max(comuna) as t FROM Clientes GROUP BY comuna ORDER BY c DESC LIMIT 1;
 
-SELECT comuna, COUNT(comuna) AS c FROM Clientes GROUP BY comuna ORDER BY c DESC LIMIT 1;
+
